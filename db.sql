a6ef93f9b6ba41d1a34b319d8daf31dcc9d629f8
BVD <Lcjsleeper@gmail.com>
2025-09-29 14:40:01 +0200

    dbsql

--- db.sql
+++ db.sql
@@ -1,9 +1,10 @@
-SET utf8mb4;
+SET NAMES utf8mb4;
 DROP DATABASE IF EXISTS `commands`;
-CREATE DATABASE `commands`;
-USE `commands`;
+CREATE DATABASE commands;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE commands;
 
-CREATE TABLE `client` (
+CREATE TABLE client (
   idClient BIGINT PRIMARY KEY AUTO_INCREMENT,
   nom varchar(255) NOT NULL,
   prenom varchar(255) NOT NULL,
@@ -11,7 +12,7 @@ CREATE TABLE `client` (
   email varchar(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE `commande` (
+CREATE TABLE commande (
   idCommande BIGINT PRIMARY KEY AUTO_INCREMENT,
   dateCommande datetime NOT NULL,
   montantTotal decimal(10,2) NOT NULL,
@@ -20,7 +21,7 @@ CREATE TABLE `commande` (
   FOREIGN KEY (idClient) REFERENCES client(idClient)
 );
 
-CREATE TABLE `lignecommande` (
+CREATE TABLE lignecommande (
   idLigneCommande BIGINT PRIMARY KEY AUTO_INCREMENT,
   idCommande BIGINT NOT NULL,
   idArticle BIGINT NOT NULL,
@@ -30,7 +31,7 @@ CREATE TABLE `lignecommande` (
     FOREIGN KEY (idArticle) REFERENCES article(idArticle)
 );
 
-CREATE TABLE `article` (
+CREATE TABLE article (
   idArticle BIGINT PRIMARY KEY AUTO_INCREMENT,
   libelle varchar(255) NOT NULL,
   prixUnitaire decimal(10,2) NOT NULL,
